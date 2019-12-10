c5a3cdf11c606de95f60c15af8645d72036934fa
Sagar Pratapsi <sagar.pratapsi@meaningfuldata.eu>
2019-12-10 20:26:12 +0000

    clean

--- 1_1.py
+++
@@ -1,9 +0,0 @@
-with open('1.in', 'r') as input_file:
-	masses = [int(line) for line in input_file]
-
-def fuel(mass):
-	return mass // 3 - 2
-
-total_fuel = sum(fuel(mass) for mass in masses)
-
-print('Total fuel is', total_fuel)
--- 1_naive.py
+++
@@ -1,9 +0,0 @@
-total = 0
-with open('1.in', 'r') as file:
-	for line in file:
-		module = int(line)
-		mass = module // 3 - 2
-		while mass > 0:
-			total += mass
-			mass = mass // 3 - 2
-print(total)
