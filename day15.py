6090772b08d71c800542100a2b866908f59f9586
Sagar Pratapsi <sagar.pratapsi@meaningfuldata.eu>
2019-12-16 02:25:48 +0000

    clean up

--- day15.py
+++ day15.py
@@ -16,12 +16,13 @@ def grid_to_string(grid):
 
 
 directions = NORTH, SOUTH, WEST, EAST = 1, 2, 3, 4
-responses = WALL, ALLOWED, OXYGEN = 0, 1, 2
+states = WALL, ALLOWED, OXYGEN = 0, 1, 2
 
 instructions = {NORTH: 1j, SOUTH: -1j, WEST: -1, EAST: 1}
 
-def find_oxy(position, state, dist=0):
-    global record
+def find_oxy(position, state, dist=0, record=None):
+    global computer
+    record = record or {}
     record[position] = (state, dist)
     for dir in directions:
         new_dist = dist + 1
@@ -35,13 +36,14 @@ def find_oxy(position, state, dist=0):
         elif new_state == OXYGEN:
             return new_pos, new_dist
         elif new_state == ALLOWED:
-            return find_oxy(new_pos, new_state, new_dist)
+            return find_oxy(new_pos, new_state, new_dist, record)
             computer.input({1: 2, 2: 1, 3: 4, 4: 3}[dir])  # Go back
-            computer.read()
+            computer.read()  # Throw away output
 
 
-def find_all_positions(position, state, dist=0):
-    global record
+def find_all_positions(position, state, dist=0, record=None):
+    global computer
+    record = record or {}
     record[position] = (state, dist)
     for dir in directions:
         new_dist = dist + 1
@@ -53,21 +55,23 @@ def find_all_positions(position, state, dist=0):
         if new_state == WALL:
             record[new_pos] = (WALL, -1)
         else:
-            find_all_positions(new_pos, new_state, new_dist)
+            find_all_positions(new_pos, new_state, new_dist, record)
             computer.input({1: 2, 2: 1, 3: 4, 4: 3}[dir])
             computer.read()
 
+    return record
+
+start = time()
 
 computer = IntCode(program)
 computer.start()
 
-record = {}
 oxy_pos, oxy_dist = find_oxy(position=0j, state=ALLOWED)
-
-record = {}
-find_all_positions(oxy_pos, state=OXYGEN)
+record = find_all_positions(oxy_pos, state=OXYGEN)
 time_to_fill = max(t[1] for t in record.values())
 
 print('Part 1:', oxy_dist)
 print('Part 2:', time_to_fill)
-print('Maze:', grid_to_string(record), sep='\n')
+# print('Maze:', grid_to_string(record), sep='\n')
+
+print(time() - start)
