f7bae53491134fc12602a414f1a7bd214e4d7d4a
spratapsi <sagar.dipak@gmail.com>
2019-12-18 12:19:05 +0100

    Update day16.py

--- day16.py
+++ day16.py
@@ -1,34 +1,54 @@
-from itertools import cycle, repeat
-from functools import partial
-from math import prod, gcd
-
-times = partial(repeat, None)
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-def pattern(n, base=(0, 1, 0, -1)):
-    p = cycle(k for k in base for _ in times(n))
-    next(p)
-    return p
-
-def FFT(signal, k):
-    return abs(sum(map(prod, zip(signal, pattern(k+1))))) % 10
+from itertools import accumulate
 
 with open('day16.in') as file:
-    signal = [int(n) for n in next(file).strip()]
+    original_signal = [int(n) for n in next(file).strip()]
 
-n = len(signal)
+def srange(start, end, length, step):
+    """Yield endpoints for slices:
+    --> (start, start + length), (start + step, start + step + length), ...
+    """
+    inf, sup = start, start + length
+    while sup <= end:
+        yield (inf, sup)
+        inf, sup = inf + step, sup + step
+    if inf < end:
+        yield (inf, min(sup, end))
+
+def sum_signal(acc, k, end, offset=0):
+    k_ = offset + k
+    pstart = k - 1
+    nstart = pstart + 2 * k_
+    pos = sum(acc[s] - acc[i] for i, s in srange(pstart, end, k_, 4*k_))
+    neg = sum(acc[s] - acc[i] for i, s in srange(nstart, end, k_, 4*k_))
+    return pos - neg
+
+def FFT(signal, offset=0):
+    end = len(signal)
+    acc = [0, *accumulate(signal)]
+    signal = [
+        abs(sum_signal(acc, k, end, offset)) % 10
+        for k in range(1, end + 1)
+    ]
+    return signal
 
 # Part 1
 
-for _ in times(100):
-    signal = [FFT(signal, k) for k in range(n)]
+signal = original_signal
+for phase in range(100):
+    signal = FFT(signal)
 
-part_1 = ''.join(str(k) for k in signal[:8])
+message1 = ''.join(str(x) for x in signal[:8])
 
 # Part 2
 
-offset = int(''.join(str(k) for k in signal[:7]))
+offset = int(''.join(str(x) for x in original_signal[:7]))
+signal = (original_signal * 10_000)[offset:]  # We don't to compute the whole FFT
 
-print('Part 1:', part_1)
+for phase in range(100):
+    print(phase)
+    signal = FFT(signal, offset=offset)
+
+message2 = ''.join(str(x) for x in signal[:8])
+
+print('Part 1:', message1)
+print('Part 2:', message2)
